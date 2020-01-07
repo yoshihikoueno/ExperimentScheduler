7362fb592806379c6db2d13e814724e6071218ac
Yoshihiko Ueno <windows7home@hotmail.co.jp>
2020-01-07 11:08:29 +0900

    changed from google protobuf to json. changed arg parser

--- main.py
+++ main.py
@@ -4,88 +4,84 @@ import logging
 import time
 import os
 import atexit
-
-from google.protobuf import text_format
+import json
+from collections import namedtuple
 
 import scheduler
 import worker_interface
 from utils import logger
 from utils import util_ops
 from web import web_interface as wi
-from protos import scheduler_config_pb2
-
-# Parse CL arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--logdir',
-                    help="Directory where all log files should be stored",
-                    required=True)
-parser.add_argument('--config',
-                    help="Config file for the scheduler", required=True)
-parser.add_argument('--public', action='store_true',
-                    help='Whether to publish web server to the local network')
-parser.add_argument('--port', help='The port to use for the web interface. '
-                    'Defaults to 5000', type=int)
-
-args = parser.parse_args()
-
 
 def load_config(config_path):
-    scheduler_config = scheduler_config_pb2.SchedulerConfig()
-    with open(config_path, 'r') as f:
-        text_format.Merge(f.read(), scheduler_config)
+    with open(config_path) as f:
+        scheduler_config = json.load(f)
+
+    scheduler_config = namedtuple(
+        'config',
+        scheduler_config.keys()
+    )(*scheduler_config.values())
 
     return scheduler_config
 
+def run(logdir, config, port, public):
+    web_port = port
+    logger.init_logger(logdir)
 
-def run():
-    if not os.path.exists(args.logdir):
+    if not os.path.exists(logdir):
+        logging.warn(f'logdir {logdir} not found. creating...')
+        os.makedirs(logdir)
         raise ValueError("Invalid logdir.")
-    if not os.path.exists(args.config):
+    if not os.path.exists(config):
         raise ValueError("Invalid config file.")
 
-    web_port = args.port if args.port else 5000
-
-    logger.init_logger(args.logdir)
-
-    config = load_config(args.config)
+    config = load_config(config)
 
     num_devices_per_worker = util_ops.get_num_devices()
     if num_devices_per_worker < 1:
-        raise ValueError("There must be atleast one GPU.")
+        raise ValueError('There must be at least one GPU.')
 
     # In hours, 0 for no limit
     experiment_time_limit = config.experiment_time_limit
     initial_tf_port = config.initial_tf_port
     # We could run up to one tf server per device on one worker, so we need to
     # have that many ports
-    tf_ports = list(range(initial_tf_port,
-                          initial_tf_port + num_devices_per_worker))
+    tf_ports = list(range(initial_tf_port, initial_tf_port + num_devices_per_worker))
     hosts = config.host_addresses
 
-    workers = dict()
-    for host in hosts:
-        workers[host] = worker_interface.WorkerInterface(
-            host=host, tf_ports=tf_ports, num_devices=num_devices_per_worker,
-            logdir=args.logdir, resource_folder=config.resource_folder,
-            docker_resource_folder=config.docker_resource_folder)
+    workers = {
+        host: worker_interface.WorkerInterface(
+            host=host,
+            tf_ports=tf_ports,
+            num_devices=num_devices_per_worker,
+            logdir=logdir,
+            resource_folder=config.resource_folder,
+            docker_resource_folder=config.docker_resource_folder,
+        ) for host in hosts
+    }
 
     experiment_scheduler = scheduler.Scheduler(
         workers=workers,
-        logdir=args.logdir, experiment_time_limit=experiment_time_limit,
-        reorganize_experiments_interval=config.reorganize_experiments_interval)
+        logdir=logdir,
+        experiment_time_limit=experiment_time_limit,
+        reorganize_experiments_interval=config.reorganize_experiments_interval,
+    )
 
     # Register shutdown callback
     atexit.register(experiment_scheduler.shutdown)
 
     web_interface = wi.WebInterface(
-        scheduler_ref=experiment_scheduler, resource_folder=config.resource_folder,
-        docker_resource_folder=config.docker_resource_folder)
+        scheduler_ref=experiment_scheduler,
+        resource_folder=config.resource_folder,
+        docker_resource_folder=config.docker_resource_folder,
+    )
 
-    public = args.public
     # Start web server thread
-    web_thread = threading.Thread(target=web_interface.run,
-                                  args=(public, web_port))
-    web_thread.daemon = True
+    web_thread = threading.Thread(
+        target=web_interface.run,
+        args=(public, web_port),
+        daemon=True,
+    )
     web_thread.start()
     logging.info('Web Interface Thread started.')
 
@@ -107,4 +103,29 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    # Parse CL arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--logdir',
+        help="Directory where all log files should be stored",
+        default='/tmp/ExperimentSchedulerTemp/log',
+    )
+    parser.add_argument(
+        '--config',
+        help="Config file for the scheduler",
+        default='config.json',
+    )
+    parser.add_argument(
+        '--public',
+        action='store_true',
+        help='Whether to publish web server to the local network',
+    )
+    parser.add_argument(
+        '--port',
+        help='The port to use for the web interface. Defaults to 5000',
+        default=5000,
+        type=int,
+    )
+
+    args = parser.parse_args()
+    run(**vars(args))
