553d2844c974ac8cf08dd10ad8bdc0b2173bed37
yoshihiko.ueno <windows7home@hotmail.co.jp>
2020-09-25 22:11:37 +0900

    fixed a issue caused by negative experiment id

--- experiment.py
+++ experiment.py
@@ -2,19 +2,24 @@ import datetime
 
 
 class Experiment():
-  def __init__(self, docker_file, name, gpu_settings,
-               use_multiple_workers, can_restart, user_name, framework):
-    self.docker_file = docker_file
-    self.name = name
-    self.gpu_settings = gpu_settings
-    self.use_multiple_workers = use_multiple_workers
-    self.can_restart = can_restart
-    self.user_name = user_name
-    # tensorflow or chainer
-    self.framework = framework
-    self.schedule_time = datetime.datetime.now()
-    self.start_time = None
-    self.finish_time = None
-    # Can be 'killed: <reason>', 'success', 'error: <code>'
-    self.finish_return_code = None
-    self.unique_id = f'{hash(self.name + self.user_name + str(self.schedule_time)): x}'
+    def __init__(self, docker_file, name, gpu_settings,
+                 use_multiple_workers, can_restart, user_name, framework):
+        self.docker_file = docker_file
+        self.name = name
+        self.gpu_settings = gpu_settings
+        self.use_multiple_workers = use_multiple_workers
+        self.can_restart = can_restart
+        self.user_name = user_name
+        # tensorflow or chainer
+        self.framework = framework
+        self.schedule_time = datetime.datetime.now()
+        self.start_time = None
+        self.finish_time = None
+        # Can be 'killed: <reason>', 'success', 'error: <code>'
+        self.finish_return_code = None
+        self.unique_id = f'{self.get_hash(): x}'
+
+    def get_hash(self):
+        value = hash(self.name + self.user_name + str(self.schedule_time))
+        value = 2 * abs(value) + (value < 0)
+        return value
