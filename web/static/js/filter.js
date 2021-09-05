5c4d9c23f1170ec23ce03cb7b45f89c6ade30aed
yoshihiko.ueno <windows7home@hotmail.co.jp>
2021-09-05 23:37:23 +0900

    Added experiment name filter

--- web/static/js/filter.js
+++ web/static/js/filter.js
@@ -1,18 +1,40 @@
-function filter_by(query, metric_name) {
-  $(`#finishedexperiments .table__drow`).filter(function () {
-    $(this).toggle(
-      $(`.table__dcell--${metric_name} p`, this).text().indexOf(query) > -1
+class KeywordFilter {
+  constructor(container, text_extractor) {
+    this.filter_results_permetric = {}
+    this.filter_results = []
+    this.container = container
+    this.text_extractor = text_extractor
+  }
+  filter(query, metric) {
+    const that = this
+    this.filter_results_permetric[metric] = $(this.container).map(function (){ return that.text_extractor(metric, this).indexOf(query) > -1 })
+    this.filter_results = Object.values(this.filter_results_permetric)[0].map(
+      (i, e) => Object.keys(this.filter_results_permetric).map(metric => this.filter_results_permetric[metric][i]).reduce((curr, acc) => curr && acc)
     )
-  })
+    this.apply()
+  }
+  apply() {
+    const that = this
+    $(this.container).map(function(i) {$(this).toggle(that.filter_results[i])})
+  }
 }
 
 $(document).ready(function (){
+  keyword_filter = new KeywordFilter(
+    `#finishedexperiments .table__drow`,
+    (metric, context) => $(`.table__dcell--${metric} p`, context).text(),
+  )
   $(".filter_box--name").on(
     "keyup",
-    function () {filter_by($(this).val(), "username");},
+    function () {keyword_filter.filter($(this).val(), "username")},
+  )
+  $(".filter_box--experiment").on(
+    "keyup",
+    function () {keyword_filter.filter($(this).val(), "experiment_name")},
   )
   $(window).on("pageshow", function (){
-    filter_by($(".filter_box--name").val(), "username")
+    keyword_filter.filter($(".filter_box--name").val(), "username")
+    keyword_filter.filter($(".filter_box--experiment").val(), "experiment_name")
   })
 })
 
